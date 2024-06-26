7a010a76fd29ae84cdcbdb2a3ac900fe6f046c2b
sudipmeyur <sudip.meyur@ltimindtree.com>
2024-06-26 13:02:55 +0530

    unnecessary comment deleted

--- src/main/java/com/example/aws/spring/AwsSpringDemoApplication.java
+++ src/main/java/com/example/aws/spring/AwsSpringDemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AwsSpringDemoApplication {
-//test
 	public static void main(String[] args) {
 		SpringApplication.run(AwsSpringDemoApplication.class, args);
 	}
